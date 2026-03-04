8e54cd865be6b56181b3b5e64a74e073d7aa19cd
shivam <shivamchouhan123sc@gmail.com>
2026-03-04 23:55:22 +0530

    JavaScript Conditionals Statements

---
+++ 01-basics/conditionals.js
@@ -0,0 +1,82 @@
+// JavaScript Conditionals Statements
+
+//  if statement
+let age = 20;
+
+if (age >= 18) {
+    console.log("You are eligible to vote");
+}
+
+
+//  if - else
+let number = 7;
+
+if (number % 2 === 0) {
+    console.log("Number is Even");
+} else {
+    console.log("Number is Odd");
+}
+
+
+// if - else if - else
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade: A");
+}
+else if (marks >= 70) {
+    console.log("Grade: B");
+}
+else if (marks >= 50) {
+    console.log("Grade: C");
+}
+else {
+    console.log("Fail");
+}
+
+
+//  Nested if
+let temperature = 30;
+
+if (temperature > 25) {
+    if (temperature < 35) {
+        console.log("Weather is Pleasant");
+    }
+}
+
+
+//  Switch Statement
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3:
+        console.log("Wednesday");
+        break;
+
+    case 4:
+        console.log("Thursday");
+        break;
+
+    case 5:
+        console.log("Friday");
+        break;
+
+    case 6:
+        console.log("Saturday");
+        break;
+
+    case 7:
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Invalid day");
+}
