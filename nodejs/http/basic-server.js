dcdb386751f81b589e96a7adb050cf946e8af4ee
shivam <shivamchouhan123sc@gmail.com>
2026-06-20 23:53:40 +0530

    Basic Server

---
+++ nodejs/http/basic-server.js
@@ -0,0 +1,22 @@
+const http=require ("http");
+const fs=require("fs");
+
+const myServer=http.createServer((req,res)=>{
+    const log=`${Date.now()}: ${req.url} New Req Received\n`
+    
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(req.url){
+            case "/":
+                res.end("home page");
+            break;
+            case "/about":
+                res.end("I am Shivam Chouhan");
+                break;
+            default:
+                res.end("404 Not Found");
+        }
+    })
+
+});
+
+myServer.listen(8000,()=> console.log("Server Started"))
