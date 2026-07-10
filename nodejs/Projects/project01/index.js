0ea8109669e48850fecf59c0f5174a485c90cae0
shivam <shivamchouhan123sc@gmail.com>
2026-07-11 00:50:25 +0530

    Validate required fields in POST/api/users

--- nodejs/Projects/project01/index.js
+++ nodejs/Projects/project01/index.js
@@ -118,6 +118,9 @@ app.route('/api/users/:id')
 app.post('/api/users',(req,res)=>{
     //To create new user
     const body=req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email ||!body.gender || !body.ip_address ||!body.job_title){
+        return res.status(400).json({msg:"Please provide all the required fields"});
+    }
     users.push({id:users.length+1,...body});
     fs.writeFile('./MOCK_DATA.json',JSON.stringify(users,null,2),(err,data)=>{
         if(err){
