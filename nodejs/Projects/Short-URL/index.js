454c44f62dc3ec19d759c21e1983456a4398cd3e
shivam <shivamchouhan123sc@gmail.com>
2026-07-18 13:42:42 +0530

    Build URL shortner

---
+++ nodejs/Projects/Short-URL/index.js
@@ -0,0 +1,37 @@
+const express=require('express');
+const {connectToMongoDB}=require('./connect');
+const URL=require('./models/url');
+const urlRoute=require('./routes/url');
+const app=express();
+const PORT=8001;
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(()=>{
+    console.log('MongoDB connected');
+})
+.catch((err)=>{
+    console.error('MongoDB connection error:', err);
+});
+
+app.use(express.json());
+app.use('/url',urlRoute);
+app.get('/:shortId',async (req,res)=>{
+    const shortId=req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {shortId,
+
+        },
+        {$push:{
+            visitHistory:{
+                timestamp: Date.now(),
+            },
+        },
+     });
+     if(!entry){
+        return res.status(404).json({error:"Short URL not found"});
+     }
+        res.redirect(entry.redirectURL);
+});
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
